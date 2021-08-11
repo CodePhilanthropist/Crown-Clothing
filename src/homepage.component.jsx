ba90959d1d29cb70f9b4a09b22c6deffc0918fc2
CodePhilanthropist <barrigaete@gmail.com>
2021-08-11 19:20:24 +0800

    men

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -29,7 +29,7 @@ const Homepage = () => (
       </div>
       <div className="menu-item">
         <div className="content">
-            <h1>Hats</h1>
+            <h1>Men</h1>
             <span>Shop now</span>
         </div>
       </div>
