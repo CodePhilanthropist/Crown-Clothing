66491c118a1b5c864efb6865dca3de552e504339
CodePhilanthropist <barrigaete@gmail.com>
2021-08-11 19:15:39 +0800

    adding divisions

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 const Homepage = () =>(
     <div className="homepage">
-        
+        <div className="directory-menu">
+            <div className="menu-item">
+            </div>
+        </div>
     </div>
 )
