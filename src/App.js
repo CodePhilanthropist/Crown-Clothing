862b35bce83019c8f66ca16051617cb97cbb4859
CodePhilanthropist <barrigaete@gmail.com>
2021-08-12 21:31:39 +0800

    Hats page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import Homepage from './pages/homepage/homepage.component'
 import "./App.css";
+import { Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return <div className="App">
