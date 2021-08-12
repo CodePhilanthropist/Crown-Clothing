cc381550388985e416c74efd48030c7392d0b59d
CodePhilanthropist <barrigaete@gmail.com>
2021-08-12 15:45:49 +0800

    declaring state

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
 class Directory extends React.Component{
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            sections: []
+        }
+    }
 }
+
+export default Directory;
