033733a4a8b2bf1b5da4eb82e9b456ca66198cef
CodePhilanthropist <barrigaete@gmail.com>
2021-08-12 15:33:48 +0800

    adding menu-item

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MenuItem = () => (
+  <div className="menu-item">
+    <div className="content">
+      <h1>Hats</h1>
+      <span>Shop now</span>
+    </div>
+  </div>
+);
