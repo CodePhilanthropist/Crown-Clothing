19c604acc71748844c16f98f7a631424dbcffad2
CodePhilanthropist <barrigaete@gmail.com>
2021-08-12 15:38:52 +0800

    exporting menu item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const MenuItem = () => (
+const MenuItem = ({title}) => (
   <div className="menu-item">
     <div className="content">
-      <h1>Hats</h1>
+      <h1>{title}</h1>
       <span>Shop now</span>
     </div>
   </div>
 );
+
+export default MenuItem;
