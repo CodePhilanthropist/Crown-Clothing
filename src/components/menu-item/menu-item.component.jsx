eae04399d4c21124703e2c367dd9cbfea56d6ad0
sampe <barrigaete@gmail.com>
2021-08-16 12:07:11 +0800

    adding linkUrl

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./menu-item.styles.scss";
 import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className="background-image"
       style={{
