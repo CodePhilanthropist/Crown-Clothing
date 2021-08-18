8ce1523be0e990a395357184b92b532a47a25030
sampe <barrigaete@gmail.com>
2021-08-18 22:22:48 +0800

    adding the capability to accept states

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export class SignIn extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+        }
+    }
     render() {
         return (
             <div>
