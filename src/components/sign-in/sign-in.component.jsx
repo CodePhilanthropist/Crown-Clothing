240ddec24677fc1f5468265e055288a4d19690a7
sampe <barrigaete@gmail.com>
2021-08-18 22:29:18 +0800

    handleSubmit rolling out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -8,12 +8,17 @@ export class SignIn extends Component {
             password: "",
         }
     }
+
+    handleSubmit = () => (
+
+    );
+
     render() {
         return (
             <div className="sign-in">
                 <h2>I already have an acount</h2>
                 <span>Sign in with your email and password</span>
-                <form action="">
+                <form action="" onSubmit={this.handleSubmit}>
                     <input type="text" className="email" name="email" value={this.state.email} required/>
                     <label htmlFor="email">Email</label>
                     <input type="password" className="password" name="password" value={this.state.password} required/>
