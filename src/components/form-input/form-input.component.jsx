144b758eadfcfc5cef22f7cbc94b47a0f62ce69c
sampe <barrigaete@gmail.com>
2021-08-19 13:13:35 +0800

    Almighty Push

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./form-input.styles.scss";
 
 export const FormInput = ({ handleChange, label, ...otherProps }) => {
   return (
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { FormInput } from '../form-input/form-input.component';
 
 export class SignIn extends Component {
     constructor(props){
