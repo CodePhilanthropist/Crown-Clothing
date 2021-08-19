6783ee90f5e38574f22e1582b63d85e8f06ccdea
sampe <barrigaete@gmail.com>
2021-08-19 13:31:52 +0800

    exporting the button to Korea

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./custom-button.styles.scss";
 
 export const CustomButton = ({ children, ...otherProps }) => {
-  return <button className="custom-button" {...otherProps}>Submit</button>;
+  return <button className="custom-button" {...otherProps}>{children}</button>;
 };
