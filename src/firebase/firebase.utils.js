a3599ec2dddd7d3fa0ddf7ea99027264249ca0a0
sampe <barrigaete@gmail.com>
2021-08-19 15:09:47 +0800

    firebase.utils.js initialization

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+
+    apiKey: "AIzaSyD8XJfrAH_KON6yghhU0GhSMyNmmylf6Mo",
+  
+    authDomain: "crown-db-eef55.firebaseapp.com",
+  
+    projectId: "crown-db-eef55",
+  
+    storageBucket: "crown-db-eef55.appspot.com",
+  
+    messagingSenderId: "197155720693",
+  
+    appId: "1:197155720693:web:f0419a98564e98f90024bc",
+  
+    measurementId: "G-S557FRBB39"
+  
+  };
+
+firebase.initializeApp(config);
