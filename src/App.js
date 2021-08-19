0a46fb4b0284eaf7999f18695431a43ac765a2f5
sampe <barrigaete@gmail.com>
2021-08-19 15:29:28 +0800

    convert to a class instead of function

--- src/App.js
+++ src/App.js
@@ -4,26 +4,35 @@ import { Route, Switch } from "react-router-dom";
 import Shop from "./pages/shop/shop.component";
 import { Header } from "./components/header/header.component";
 import { SignInAndOut } from "./pages/sign-in-sign-out/sign-in-and-out.component";
+import { auth } from "./firebase/firebase.utils";
+import { Component } from "react";
 
 const HatsPage = (props) => (
   <div>
     <h1>Hats Page</h1>
-    <button onClick={() => props.history.push('/tongkoy')}>Click me</button>
+    <button onClick={() => props.history.push("/tongkoy")}>Click me</button>
   </div>
 );
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/hat" component={HatsPage} />
-        <Route exact path="/shop" component={Shop} />
-        <Route exact path="/sign-in" component={SignInAndOut} />
-      </Switch>
-    </div>
-  );
+class App extends Component() {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/hat" component={HatsPage} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/sign-in" component={SignInAndOut} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
