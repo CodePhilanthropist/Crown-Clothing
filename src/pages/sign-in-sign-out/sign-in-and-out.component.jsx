f94e613c91bd87317ba76d40f00a0422d9cdb7df
sampe <barrigaete@gmail.com>
2021-08-20 22:30:13 +0800

    importing the signup component

--- src/pages/sign-in-sign-out/sign-in-and-out.component.jsx
+++ src/pages/sign-in-sign-out/sign-in-and-out.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./sign-in-and-out.styles.scss";
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 export const SignInAndOut = () => {
     return (
         <div>
