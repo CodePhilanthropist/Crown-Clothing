c6d1b6657edea7f757fbe5867d03704c67eb98a1
sampe <barrigaete@gmail.com>
2021-08-20 21:28:24 +0800

    almighty push

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -29,18 +29,20 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!snapShot.exists){
         const {displayName, email} = userAuth;
         const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch(error){
+            console.log("error creating user", error);
+        }
     }
 
-    try{
-        await userRef.set({
-            displayName,
-            email,
-            createdAt,
-            ...additionalData
-        })
-    }catch(error){
-        console.log("error creating user", error);
-    }
+   
 
 };
 
