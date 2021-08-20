5dd84550083b902a620d5e5f1cb77342e8572879
sampe <barrigaete@gmail.com>
2021-08-20 17:42:29 +0800

    pushing nani

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./header.styles.scss";
-import {Link} from "react-router-dom";
-import {ReactComponent as Logo} from "../../assets/crown.svg";
-import { auth } from '../../firebase/firebase.utils';
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
-export const Header = ({currentUser}) => {
-    return (
-        <div className="header">
-            <Link to="/" className="logo-container">
-                <Logo className="logo"/>
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">Shop</Link>
-                <Link className="option" to="/shop">Contact</Link>
-                <Link className="option" to="/shop">{currentUser}</Link>
-            </div>
-        </div>
-    )
-}
+export const Header = ({ currentUser }) => {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/shop">
+          Contact
+        </Link>
+        <Link className="option" to="/shop">
+          {currentUser ? <div></div> : <Link></Link>}
+        </Link>
+      </div>
+    </div>
+  );
+};
