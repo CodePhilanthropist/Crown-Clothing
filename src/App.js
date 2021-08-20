028771260234c06e6a26ef83ac3ef6e73dc6476e
sampe <barrigaete@gmail.com>
2021-08-20 22:51:11 +0800

    adding all changes

--- src/App.js
+++ src/App.js
@@ -7,38 +7,35 @@ import { SignInAndOut } from "./pages/sign-in-sign-out/sign-in-and-out.component
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import React from "react";
 
-const HatsPage = (props) => (
-  <div>
-    <h1>Hats Page</h1>
-    <button onClick={() => props.history.push("/tongkoy")}>Click me</button>
-  </div>
-);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentUser: null,
+    };
   }
 
   unSubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot((snapShot) => {
-          this.setState(
-            { 
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
             }
-          );
+          });
+
+          console.log(this.state);
         });
-      } else {
-        this.setState({ currentUser: userAuth });
       }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/sign-in-sign-out/sign-in-and-out.component.jsx
+++ src/pages/sign-in-sign-out/sign-in-and-out.component.jsx
@@ -4,7 +4,7 @@ import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 export const SignInAndOut = () => {
     return (
-        <div>
+        <div className="sign-in-and-sign-up">
             <SignIn/>
             <SignUp/>
         </div>
