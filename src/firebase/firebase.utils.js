4b67fc87c8ba5cedbbee98332b2f8d199d2b188c
sampe <barrigaete@gmail.com>
2021-08-20 21:17:06 +0800

    almighty push

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -23,7 +23,7 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
-    const userRef = firestore.doc('rian');
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
     const snapShot = await userRef.get();
 
     console.log(snapShot);
