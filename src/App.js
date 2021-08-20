df1352f7b923054aaebfea67aeec49ae74b57198
sampe <barrigaete@gmail.com>
2021-08-20 21:46:01 +0800

    settign state

--- src/App.js
+++ src/App.js
@@ -28,7 +28,12 @@ class App extends React.Component {
      if(!userAuth){
        const userRef = await createUserProfileDocument(userAuth);
        userRef.onSnapshot(snapShot => {
-         console.log(snapShot.data());
+         this.setState({
+           currentUser: {
+             id: snapShot.id,
+             ...snapShot.data()
+           }
+         })
        })
      }
       
