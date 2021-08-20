00d5c738653e73f8807fef896489d45fddb130cd
sampe <barrigaete@gmail.com>
2021-08-20 21:22:31 +0800

    chaning to additionalData

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -36,7 +36,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
             displayName,
             email,
             createdAt,
-            ...otherProps
+            ...additionalData
         })
     }catch(error){
 
