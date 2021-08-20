a8d54b6d811b6f4e960d2cd5dec83a4732f8e028
sampe <barrigaete@gmail.com>
2021-08-20 17:45:36 +0800

    adding to to Link

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,7 +18,7 @@ export const Header = ({ currentUser }) => {
           Contact
         </Link>
         <Link className="option" to="/shop">
-          {currentUser ? <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div> : <Link to="/"></Link>}
+          {currentUser ? <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div> : <Link className="option" to="/"></Link>}
         </Link>
       </div>
     </div>
