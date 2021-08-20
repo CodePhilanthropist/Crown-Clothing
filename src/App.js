0f5443b18eddfea9e27fed09bd4d86c53e8c6f54
sampe <barrigaete@gmail.com>
2021-08-20 21:48:03 +0800

    moving the console log

--- src/App.js
+++ src/App.js
@@ -34,8 +34,9 @@ class App extends React.Component {
              ...snapShot.data()
            }
          });
-         console.log(this.state);
+         
        })
+       console.log(this.state);
      }
       
     });
