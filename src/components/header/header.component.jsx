c517d8331076eddfebf72bbfb48c44a0c1e81f91
sampe <barrigaete@gmail.com>
2021-08-20 17:41:00 +0800

    importing currentUser

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import "./header.styles.scss";
 import {Link} from "react-router-dom";
 import {ReactComponent as Logo} from "../../assets/crown.svg";
+import { auth } from '../../firebase/firebase.utils';
 
-export const Header = () => {
+export const Header = ({currentUser}) => {
     return (
         <div className="header">
             <Link to="/" className="logo-container">
