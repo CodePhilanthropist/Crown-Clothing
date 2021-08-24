9d4120f694e70b18ada39bd8fb703968942acbc6
sampe <barrigaete@gmail.com>
2021-08-25 01:13:18 +0800

    adding store then assigning it as createStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,4 +4,4 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
-const store = createStore()
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
