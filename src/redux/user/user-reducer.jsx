0563ba1a4f2df19cd014d8f980875c28754d2d19
sampe <barrigaete@gmail.com>
2021-08-25 00:54:38 +0800

    currentUser: action.payload

--- src/redux/user/user-reducer.jsx
+++ src/redux/user/user-reducer.jsx
@@ -6,6 +6,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
         case "SET_CURRENT_USER":
             return {
                 ...state,
+                currentUser: action.payload
             }
         default:
             return state;
