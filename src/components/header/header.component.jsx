966b02bd94a241e6a3ab28903441b801e471a46a
sampe <barrigaete@gmail.com>
2021-08-25 15:53:52 +0800

    importing cartIcon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 
 import {connect} from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+
 
 const Header = ({ currentUser }) => {
   return (
