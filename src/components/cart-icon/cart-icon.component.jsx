811d7e1a81aedd0e1144b54baac28b5bc7a84fd5
sampe <barrigaete@gmail.com>
2021-08-25 15:49:37 +0800

    adding shopping icon class

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,8 +5,8 @@ import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 const CartIcon = () => {
     return (
         <div className="cart-icon">
-            <ShoppingIcon/>
-            <span>0</span>
+            <ShoppingIcon className="shopping-icon"/>
+            <span className="item-count">0</span>
         </div>
     )
 }
