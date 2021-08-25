e35a591fca3d0383e35b5ce333bc270263b55ea5
sampe <barrigaete@gmail.com>
2021-08-25 17:24:46 +0800

    state.actions

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -15,7 +15,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state, cartItems ]
+        cartItems: [...state.cartItems, ]
       }
     default:
       return state;
