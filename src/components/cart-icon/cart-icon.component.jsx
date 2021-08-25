82759730a824735eea1e1dfa735899df7b91ff27
sampe <barrigaete@gmail.com>
2021-08-25 15:47:44 +0800

    importing shopping bag svg

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./cart-icon.styles.scss";
+import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 
 const CartIcon = () => {
     return (
