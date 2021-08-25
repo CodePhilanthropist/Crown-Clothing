02ddee202771427a31b0460b21edeb33d33b5d35
sampe <barrigaete@gmail.com>
2021-08-25 17:34:15 +0800

    format document

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./collection-item.styles.scss";
-import { CustomButton } from '../custom-button/custom-button.component';
-import {connect} from "react-redux";
-import {addItem} from "../../redux/cart/cart.actions";
+import { CustomButton } from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 
-
-const CollectionItem = ({id, name, price, imageUrl}) => {
-    return (
-        <div className="collection-item">
-            <div className="image" style={{
-                backgroundImage: `url(${imageUrl})`,
-
-            }}/>
-                <div className="collection-footer">
-                    <span className="name">{name}</span>
-                    <span className="price">{price}</span>
-                </div>
-                <CustomButton inverted>Add to Cart</CustomButton>
-        </div>
-    )
-}
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted>Add to Cart</CustomButton>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
-    addItem: item => dispatch(addItem(item)),
+  addItem: (item) => dispatch(addItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
