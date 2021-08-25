4d208a7d3ea2e29cb3e8743f6d7ffce781931538
sampe <barrigaete@gmail.com>
2021-08-25 17:04:39 +0800

    adding inverted class

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,7 +4,7 @@ import "./custom-button.styles.scss";
 export const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
   return (
     <button
-      className={` ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+      className={` ${inverted ? "inverted" : ""} ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
       {...otherProps}
     >
       {children}
