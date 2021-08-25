cb7ea441ec6e2c6d368e996699ddd7fe1f201df1
sampe <barrigaete@gmail.com>
2021-08-25 16:53:26 +0800

    fix something in CArt Icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 
 import {connect} from "react-redux";
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
+
 const CartIcon = ({toggleCartHidden}) => {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
