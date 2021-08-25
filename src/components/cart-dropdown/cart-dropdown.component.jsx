8618f437f4440c942cb41d5bdf8c61e5d571a904
sampe <barrigaete@gmail.com>
2021-08-25 16:01:23 +0800

    importing custom button

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {CustomButton} from "../custom-button/custom-button.component";
+
+const CartDropdpwn = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CartDropdpwn
