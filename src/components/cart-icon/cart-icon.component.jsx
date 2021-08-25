fb6447a2466704349089645c0b82e93575e6d0e5
sampe <barrigaete@gmail.com>
2021-08-25 15:46:35 +0800

    import cart icon styles

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./cart-icon.styles.scss";
+
+const CartIcon = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CartIcon
