804b5ae1792c7f51192a0ce530c0ba9a056f257d
sampe <barrigaete@gmail.com>
2021-08-26 02:13:07 +0800

    wrapping with find() function to filter

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,3 @@
 export const addItemToCart = (cartitems, cartItemToAdd) => {
-    const existingCartItems = cartitems.find()   
+    const existingCartItems = cartitems.find(cartItem => cartItem.id == cartItemToAdd.id)   
 }
