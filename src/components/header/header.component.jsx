e204b4ef605db7f057a1e950caebd0677f5da195
sampe <barrigaete@gmail.com>
2021-08-25 16:07:52 +0800

    importing cart dropdown component to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,7 @@ import { auth } from "../../firebase/firebase.utils";
 
 import {connect} from "react-redux";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 
 const Header = ({ currentUser }) => {
