33e1a20e64e16de3b32b533f4840ad93932a4f85
sampe <barrigaete@gmail.com>
2021-08-26 02:14:13 +0800

    checking if existingCartItems exists

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,4 +2,8 @@ export const addItemToCart = (cartitems, cartItemToAdd) => {
   const existingCartItems = cartitems.find(
     (cartItem) => cartItem.id == cartItemToAdd.id
   );
+
+  if (existingCartItems){
+      
+  }
 };
