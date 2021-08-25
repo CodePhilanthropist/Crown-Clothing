0c45f1547f544f05749622190ff522106dd651f6
sampe <barrigaete@gmail.com>
2021-08-25 15:48:31 +0800

    adding span to represen the total number of items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,8 +4,9 @@ import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 
 const CartIcon = () => {
     return (
-        <div>
-            
+        <div className="cart-icon">
+            <ShoppingIcon/>
+            <span>0</span>
         </div>
     )
 }
