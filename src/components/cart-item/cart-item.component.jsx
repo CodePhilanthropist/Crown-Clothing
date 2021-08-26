2ac9da3f93d7aaef76087465610fa404dd252f19
sampe <barrigaete@gmail.com>
2021-08-26 16:00:55 +0800

    adding item details

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,6 +5,9 @@ const Cartitem = ({item: {imageUrl, price, name }}) => {
     return (
         <div className="cart-item">
             <img src={imageUrl} alt="item" />
+            <div className="item-details">
+                
+            </div>
         </div>
     )
 }
