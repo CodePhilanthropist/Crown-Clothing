9635c8fc01209e501ae52e5bd238c8a2c86ec1cf
sampe <barrigaete@gmail.com>
2021-08-26 22:25:39 +0800

    const mapStateToProps

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -18,4 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = ({cart: {cartItems}}) = ({
+    
+});
+
 export default connect(null, mapDispatchToProps)(CartIcon);
