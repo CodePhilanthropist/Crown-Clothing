e51f51dbf20245953961ec374a9b4f383a975f39
sampe <barrigaete@gmail.com>
2021-08-26 16:09:23 +0800

    passing cart item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {CustomButton} from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
-import Cartitem from '../cart-item/cart-item.component';
+import CartItem from '../cart-item/cart-item.component';
 import {connect} from "react-redux";
 
-const CartDropdown = ({Cart}) => {
+const CartDropdown = ({CartItem}) => {
     return (
         <div className="cart-dropdown">
             <div className="cart-items">
