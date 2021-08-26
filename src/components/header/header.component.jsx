c3c4758e0dfa8415f5b0e2ccf091559a9a4c5d01
sampe <barrigaete@gmail.com>
2021-08-27 03:14:50 +0800

    change something in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -35,9 +35,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: {hidden} }) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
-  hidden,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
