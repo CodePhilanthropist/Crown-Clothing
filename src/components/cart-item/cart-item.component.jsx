a8c27221ae2607485b07b4551d48c721dc057f23
sampe <barrigaete@gmail.com>
2021-08-26 16:02:42 +0800

    dollar sign for the price

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,20 +1,16 @@
-import React from 'react';
+import React from "react";
 import "./cart-item.styles.scss";
 
-const Cartitem = ({item: {imageUrl, price, name }}) => {
-    return (
-        <div className="cart-item">
-            <img src={imageUrl} alt="item" />
-            <div className="item-details">
-                <span className="name">
-                    {name}
-                </span>
-                <span className="price">
-                    {price}
-                </span>
-            </div>
-        </div>
-    )
-}
+const Cartitem = ({ item: { imageUrl, price, name } }) => {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">${price}</span>
+      </div>
+    </div>
+  );
+};
 
-export default Cartitem
+export default Cartitem;
