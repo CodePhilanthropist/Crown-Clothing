0cbb87a9653851953c600f4f5068bdeb97697730
sampe <barrigaete@gmail.com>
2021-08-26 16:01:15 +0800

    adding the span

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -6,7 +6,9 @@ const Cartitem = ({item: {imageUrl, price, name }}) => {
         <div className="cart-item">
             <img src={imageUrl} alt="item" />
             <div className="item-details">
-                
+                <span className="name">
+                    
+                </span>
             </div>
         </div>
     )
