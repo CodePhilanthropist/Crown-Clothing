a5acfd86f89338619f6db8003e9afb0b0477f8f7
sampe <barrigaete@gmail.com>
2021-08-28 06:53:44 +0800

    image-container

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,8 +3,10 @@ import "./checkout-item.styles.scss";
 
 const CheckoutItem = () => {
     return (
-        <div>
-            
+        <div className="checkout-item">
+            <div className="image-container">
+                
+            </div>
         </div>
     )
 }
