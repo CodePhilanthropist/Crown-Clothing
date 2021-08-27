b8b805950ffafca412829a87339c894eee15d1cb
sampe <barrigaete@gmail.com>
2021-08-27 17:46:23 +0800

    import createstructureselector

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import {selectCurrentUser} from "./redux/user/user-selector";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
