640b06dcd77f638057d790b22b97a7875cde3620
sampe <barrigaete@gmail.com>
2021-08-27 17:39:13 +0800

    import select current user

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,6 +10,7 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import { createStructuredSelector } from "reselect";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user-selector";
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
