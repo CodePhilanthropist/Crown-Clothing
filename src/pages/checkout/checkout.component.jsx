c1a04a0215f37215648fc4f0c4b43c7c032841c1
sampe <barrigaete@gmail.com>
2021-08-27 18:15:46 +0800

    change to CheckoutPage

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./checkout.styles.scss";
 
-const Checkout = () => {
+const CheckoutPage = () => {
     return (
         <div>
             
@@ -9,4 +9,4 @@ const Checkout = () => {
     )
 }
 
-export default Checkout
+export default CheckoutPage;
