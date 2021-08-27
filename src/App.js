4b0c2a3af784b1fb271d28abc7b649395c9e346c
sampe <barrigaete@gmail.com>
2021-08-27 17:45:16 +0800

    user selecor in app js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SignInAndOut from "./pages/sign-in-sign-out/sign-in-and-out.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import {selectCurrentUser} from "./redux/user/user-selector";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
