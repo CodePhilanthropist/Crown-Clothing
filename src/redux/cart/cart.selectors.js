18f5de8ae1f777205061a4bbdd619db1df7cb7ef
sampe <barrigaete@gmail.com>
2021-08-27 18:41:24 +0800

    export const selectCartTotal

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,7 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     (cartItems) => cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
 )
+
+export const selectCartTotal = createSelector(
+    
+)
