de049d0c6c2483b446fc7bb02a6e8242e3244d32
sampe <barrigaete@gmail.com>
2021-08-28 20:08:17 +0800

    change the quantity

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -13,9 +13,11 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <div className="arrow">&#10094;</div>
-      <span className="value">{quantity}</span>
-      <div className="arrow">&#10095;</div>
+      <div className="quantity">
+        <div className="arrow">&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow">&#10095;</div>
+      </div>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
