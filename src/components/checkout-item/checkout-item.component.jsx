c76a0357400940e8d5efafc261c9b9894ab28581
sampe <barrigaete@gmail.com>
2021-08-28 22:24:32 +0800

    utilize addItem and removeItem

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -28,6 +28,8 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: item.dispatch(addItem(item)),
+  removeItem: item.dispatch(removeItem(item))
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
