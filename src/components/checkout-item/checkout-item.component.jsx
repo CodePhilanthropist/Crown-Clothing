0a2f07a0d45715e09169527f9f46a0b53ad25840
sampe <barrigaete@gmail.com>
2021-08-28 17:03:17 +0800

    fix the bug

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./checkout-item.styles.scss";
-
 import { connect } from "react-redux";
 
 import { clearItemFromCart } from "../../redux/cart/cart.actions";
 
+import "./checkout-item.styles.scss";
+
 const CheckoutItem = ({ cartItem, clearItem }) => {
-  const {name, imageUrl, price, quantity} = cartItem;
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -15,7 +15,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button" onClick={()=> clearItem(cartItem)}>&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
