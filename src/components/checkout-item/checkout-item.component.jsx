bdce6b93e8f687ddd4fc8da2370705872f368ce6
sampe <barrigaete@gmail.com>
2021-08-28 22:26:48 +0800

    onClick additem

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -20,7 +20,7 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       <div className="quantity">
         <div className="arrow" onClick={()=> removeItem(cartItem)}>&#10094;</div>
         <span className="value">{quantity}</span>
-        <div className="arrow">&#10095;</div>
+        <div className="arrow" onClick={()=> addItem(cartItem)}>&#10095;</div>
       </div>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
