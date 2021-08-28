186cf53ec698f6e74f47cd466c456652681cb3a0
sampe <barrigaete@gmail.com>
2021-08-28 17:12:16 +0800

    semi fix

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { FormInput } from '../form-input/form-input.component';
+import FormInput from '../form-input/form-input.component';
 import "./sign-in.styles.scss";
 import { CustomButton } from '../custom-button/custom-button.component';
 import { auth ,signInWithGoogle } from '../../firebase/firebase.utils';
