dc5870e724d9f58f4623aa0e781b42154f5024fd
sampe <barrigaete@gmail.com>
2021-08-29 19:46:12 +0800

    import createStructureSelector

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../../components/preview-collection/preview-collection.component";
 import { selectShopItems } from "../../redux/shop/shop.selectors";
