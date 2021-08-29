deb904002928cf15b7c8a6ec253cf0649dac580d
sampe <barrigaete@gmail.com>
2021-08-29 19:15:30 +0800

    exporting to root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root",
