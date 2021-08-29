32112abd9b79343af7d7bedea8be9ea68ab2e963
sampe <barrigaete@gmail.com>
2021-08-29 19:38:37 +0800

    removing the constructor

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 import CollectionPreview from "../../components/preview-collection/preview-collection.component";
 
 export default class Shop extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: shopData,
-    };
-  }
+  
   render() {
     const { collections } = this.state;
     return (
