f6a8587b7af32ef1791579afeea48b814dc92568
sampe <barrigaete@gmail.com>
2021-08-30 02:46:54 +0800

    bug fix

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -6,6 +6,7 @@ import { createStructuredSelector } from "reselect";
 import { selectCollections } from "../../redux/shop/shop.selectors";
 
 import "./collections-overview.styles.scss";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import {route} from "react-router-dom";
+import { route } from "react-router-dom";
 
 import collectionsOverview from "../../components/collections-overview/collections-overview.component";
-
 
 const ShopPage = () => {
   return (
     <div className="shop-page">
-      <Route component={collectionsOverview}/>
+      <Route component={collectionsOverview} />
     </div>
   );
 };
-
-
 
 export default ShopPage;
