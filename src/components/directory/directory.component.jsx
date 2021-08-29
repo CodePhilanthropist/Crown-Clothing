75574309e62c8d285d95cc5487f7c00ed394c31b
sampe <barrigaete@gmail.com>
2021-08-29 19:17:05 +0800

    format code

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./directory.styles.scss";
 import MenuItem from "../menu-item/menu-item.component";
 
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 class Directory extends React.Component {
   constructor(props) {
@@ -48,8 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({id, ...otherSectionProps}) => (
-          <MenuItem key={id} {...otherSectionProps}/>
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
