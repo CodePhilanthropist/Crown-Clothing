2474c708d34a063e951d603fae64930e4e19a266
sampe <barrigaete@gmail.com>
2021-08-30 02:33:39 +0800

    importing route

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import {route} from "react-router-dom";
 
 import CollectionPreview from "../../components/preview-collection/preview-collection.component";
+
 
 const ShopPage = () => {
   return (
