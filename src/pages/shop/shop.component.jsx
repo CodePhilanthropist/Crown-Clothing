4007f3c8e7623837a7cf79318e400f609766f3c0
sampe <barrigaete@gmail.com>
2021-08-29 19:41:50 +0800

    format code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CollectionPreview from "../../components/preview-collection/preview-collection.component";
 
-const ShopPage = ({collections}) => {
-    return (
-      <div className="shop-page">
-        {collections.map(({id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-}
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
 export default ShopPage;
