684972e845c52c743bce2e4b5cf1267011d876fa
sampe <barrigaete@gmail.com>
2021-08-29 20:21:37 +0800

    fix bug

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -5,14 +5,16 @@ import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview">
-        {items.filter((item, index) => index < 4).map((item) => (
-          <CollectionItem key={item.id} item={item}/>
+    <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
     </div>
+  </div>
   );
 };
 
