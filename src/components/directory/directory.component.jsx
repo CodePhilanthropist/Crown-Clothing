1f3e88e73a1df7e273159d0c96bddcaf196bcc46
sampe <barrigaete@gmail.com>
2021-08-29 19:19:42 +0800

    passing section props

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,7 +4,7 @@ import MenuItem from "../menu-item/menu-item.component";
 
 import { connect } from "react-redux";
 
-const Directory = () => {
+const Directory = ({sections}) => {
   return (
     <div className="directory-menu">
       {sections.map(({ id, ...otherSectionProps }) => (
