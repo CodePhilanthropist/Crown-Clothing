587d0c56c671c92519eb51abde908dd6d6e7ccc6
sampe <barrigaete@gmail.com>
2021-08-29 18:40:21 +0800

    format code

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,12 +5,12 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
-    key: "root",
-    storage,
-    whitelist: ['cart']
-}
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
 export default combineReducers({
-    user: userReducer,
-    cart: cartReducer,
-})
+  user: userReducer,
+  cart: cartReducer,
+});
