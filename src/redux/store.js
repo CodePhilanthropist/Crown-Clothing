cfacf527d5c817f3a54ab1e299bfc055062e2079
sampe <barrigaete@gmail.com>
2021-08-29 18:45:52 +0800

    returning to the same era

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,6 @@ import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-export const persistor = persistStore(store);
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 export default { store, persistor };
