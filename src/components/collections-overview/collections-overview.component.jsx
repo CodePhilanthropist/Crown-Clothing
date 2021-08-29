58e02ba56cf7a3e3f6cbed779bd4583044f8b053
sampe <barrigaete@gmail.com>
2021-08-29 20:00:47 +0800

    add a mapped collections

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -7,7 +7,9 @@ import { createStructuredSelector } from 'reselect';
 const CollectionsOverview = ({collections}) => {
     return (
         <div className="collections-overview">
-            
+            {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
         </div>
     )
 }
