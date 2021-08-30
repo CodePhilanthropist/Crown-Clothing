152ae0e985bafb7a10265339ab493264e4afd154
sampe <barrigaete@gmail.com>
2021-08-30 21:47:29 +0800

    change naming convention

--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -4,7 +4,7 @@ import "./category.styles.scss";
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const Category = ({match}) => {
+const CollectionPage = ({match}) => {
     return (
         <div className="category">
             <h1>abobot</h1>
