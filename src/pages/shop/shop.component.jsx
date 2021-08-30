9db34b5564cfef16a858ada8009896f91c468db2
sampe <barrigaete@gmail.com>
2021-08-30 21:49:41 +0800

    change the one in collection Id

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,11 @@ import { Route } from "react-router-dom";
 import collectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../category/category.component";
 
-const ShopPage = ({match}) => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={collectionsOverview} />
-      <Route path={`${match.path}/:categoryId`} component={CollectionPage}/>
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
