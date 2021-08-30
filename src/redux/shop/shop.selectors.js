4c68949363208572026b96e0c3f668038d30761e
sampe <barrigaete@gmail.com>
2021-08-30 23:49:13 +0800

    remove collection ID map

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 
 const selectShop = (state) => state.shop;
 
