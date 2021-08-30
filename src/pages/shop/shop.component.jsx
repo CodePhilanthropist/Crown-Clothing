79a0b6bc43e9f269d6510e562725f8429de1443f
sampe <barrigaete@gmail.com>
2021-08-30 21:44:04 +0800

    passing category as component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import collectionsOverview from "../../components/collections-overview/collections-overview.component";
+import Category from "../category/category.component";
 
 const ShopPage = ({match}) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={collectionsOverview} />
-      <Route path={`${match.path}/:categoryId`}/>
+      <Route path={`${match.path}/:categoryId`} component={Category}/>
     </div>
   );
 };
