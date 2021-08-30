03e1d26cd91b57185e8a8924a9ad4d115012dd43
sampe <barrigaete@gmail.com>
2021-08-30 21:50:34 +0800

    collection id

--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -7,7 +7,7 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 const CollectionPage = ({match}) => {
     return (
         <div className="category">
-            <h1>abobot</h1>
+            <h1>collection page</h1>
         </div>
     )
 }
