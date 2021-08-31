e8750e3bfc3bd0dcfbace69e705f49196c22625b
sampe <barrigaete@gmail.com>
2021-08-31 19:45:33 +0800

    add selectCollections

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,7 +9,7 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollectionsForPreview = createSelector({
-  
+  [selectCollections],
 })
 
 export const selectCollection = (collectionUrlParam) =>
