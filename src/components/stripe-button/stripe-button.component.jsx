784c0b6c4501ca46f567100d45746f61b47ce1f0
sampe <barrigaete@gmail.com>
2021-09-03 21:14:02 +0800

    add pubilshable key

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,6 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
+    const publishableKey = "pk_test_51JVJ6wBDiM79ZwHNmCIq0xYV6g2Codxs4MKnvtDJYiXaQeezCp9KlWPwiTvDknNEQ44o1BVtCZRcJVnk7UmvQxjQ007UeWmrhz";
     return (
         <div>
             
