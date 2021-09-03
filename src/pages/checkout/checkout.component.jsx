f3a707d4f3a450a879b90d5a4b1d235edbe8f3c7
sampe <barrigaete@gmail.com>
2021-09-03 21:49:19 +0800

    returned the total

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -34,6 +34,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
+    <div className="total">TOTAL: ${total}</div>
     <div className="test-warning">
       *Please use the following credit card for payment
       <br />
