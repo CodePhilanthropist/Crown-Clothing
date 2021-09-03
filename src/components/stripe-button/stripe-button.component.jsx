79a4612da95b50a6c8b7d5ce9bf9826a63e2db8d
sampe <barrigaete@gmail.com>
2021-09-03 21:18:36 +0800

    add the billing address and the shipping address

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,7 +5,7 @@ const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51JVJ6wBDiM79ZwHNmCIq0xYV6g2Codxs4MKnvtDJYiXaQeezCp9KlWPwiTvDknNEQ44o1BVtCZRcJVnk7UmvQxjQ007UeWmrhz";
-  return <StripeCheckout lalbel="Pay Now" name="Crown Clothing Co" />;
+  return <StripeCheckout lalbel="Pay Now" name="Crown Clothing Co" billingAddress shippingAddress/>;
 };
 
 export default StripeCheckoutButton;
