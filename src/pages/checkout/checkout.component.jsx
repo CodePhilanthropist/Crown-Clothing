d55e3d4134923ebc14a09617be4df59dbbeb8131
sampe <barrigaete@gmail.com>
2021-09-03 21:45:48 +0800

    add an instruction for the dummy card

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,7 +35,9 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="test-warning">
-      
+      *Please use the following credit card for payment
+      <br />
+      4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
     </div>
     <StripeCheckoutButton price={total}/>
   </div>
