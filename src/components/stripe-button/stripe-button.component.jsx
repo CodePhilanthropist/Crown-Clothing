72e1101ce5f9c0f69f018b33f9d3c35ab762e18e
sampe <barrigaete@gmail.com>
2021-09-03 21:24:51 +0800

    add onToken method

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,6 +5,9 @@ const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51JVJ6wBDiM79ZwHNmCIq0xYV6g2Codxs4MKnvtDJYiXaQeezCp9KlWPwiTvDknNEQ44o1BVtCZRcJVnk7UmvQxjQ007UeWmrhz";
+    const onToken = token =>{
+        console.log(token)
+    }
   return (
     <StripeCheckout
       lalbel="Pay Now"
