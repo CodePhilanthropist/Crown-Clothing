52eb738b57ab04649a90348c3492ccb70192a5e9
sampe <barrigaete@gmail.com>
2021-09-03 21:15:56 +0800

    it's now a singular component

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,7 +5,7 @@ const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = "pk_test_51JVJ6wBDiM79ZwHNmCIq0xYV6g2Codxs4MKnvtDJYiXaQeezCp9KlWPwiTvDknNEQ44o1BVtCZRcJVnk7UmvQxjQ007UeWmrhz";
     return (
-        <StripeCheckout>
+        <StripeCheckout/>
             
     )
 }
