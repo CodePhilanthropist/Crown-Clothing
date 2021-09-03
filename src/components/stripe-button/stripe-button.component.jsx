117ca921acd1acb673dc3eeaee1c808c2f9ceb18
sampe <barrigaete@gmail.com>
2021-09-03 21:18:04 +0800

    format code

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const StripeCheckoutButton = ({price}) => {
-    const priceForStripe = price * 100;
-    const publishableKey = "pk_test_51JVJ6wBDiM79ZwHNmCIq0xYV6g2Codxs4MKnvtDJYiXaQeezCp9KlWPwiTvDknNEQ44o1BVtCZRcJVnk7UmvQxjQ007UeWmrhz";
-    return (
-        <StripeCheckout lalbel="Pay Now" name="Crown Clothing Co"/>
-            
-    )
-}
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JVJ6wBDiM79ZwHNmCIq0xYV6g2Codxs4MKnvtDJYiXaQeezCp9KlWPwiTvDknNEQ44o1BVtCZRcJVnk7UmvQxjQ007UeWmrhz";
+  return <StripeCheckout lalbel="Pay Now" name="Crown Clothing Co" />;
+};
 
 export default StripeCheckoutButton;
