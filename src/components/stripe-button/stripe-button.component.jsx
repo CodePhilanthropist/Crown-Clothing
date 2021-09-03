18c467514b5ab146dfd875e1616f5e8509afc081
sampe <barrigaete@gmail.com>
2021-09-03 21:13:22 +0800

    add git price

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
     return (
         <div>
             
