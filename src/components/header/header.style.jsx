64c06044cbe60c34e867c66bb7bea0ff514e9fde
sampe <barrigaete@gmail.com>
2021-09-08 00:19:30 +0800

    styling the options container

--- src/components/header/header.style.jsx
+++ src/components/header/header.style.jsx
@@ -16,5 +16,9 @@ export const LogoContainer = styled(Link)`
 `;
 
 export const OptionsContainer = styled.div`
-
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
