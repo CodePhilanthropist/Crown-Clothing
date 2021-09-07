e039b4350c5ecc4ff7802afe7e4bbaad354bbe36
sampe <barrigaete@gmail.com>
2021-09-08 02:44:11 +0800

    fix bugs

--- src/components/header/header.style.jsx
+++ src/components/header/header.style.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 const OptionContainerStyles = css`
   padding: 10px 15px;
@@ -14,6 +13,11 @@ export const HeaderContainer = styled.div`
   margin-bottom: 25px;
 `;
 
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  width: 70px;
+  padding: 25px;
+`;
 
 export const OptionsContainer = styled.div`
   width: 50%;
