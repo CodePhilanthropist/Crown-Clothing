df6a20454807c717007487428d48516b7922a755
sampe <barrigaete@gmail.com>
2021-09-08 11:47:16 +0800

    add invertedButton

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,7 +1,14 @@
 import styled, { css } from "styled-components";
 
 const invertedButtonStyles = css`
-    
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  &:hover {
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+  }
 `;
 
 export const CustomButtonContainer = styled.button`
