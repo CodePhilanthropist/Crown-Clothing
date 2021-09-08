bf177615b1a54ca7dd076e961c17609af0765a14
sampe <barrigaete@gmail.com>
2021-09-08 11:58:25 +0800

    remove className

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { CustomButtonContainer,  } from "./custom-button.styles";
+import { CustomButtonContainer } from "./custom-button.styles";
 
-export const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
-  return (
-    <button
-      className={` ${inverted ? "inverted" : ""} ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+export const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => {
+  return <CustomButtonContainer>{children}</CustomButtonContainer>;
 };
