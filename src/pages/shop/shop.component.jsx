a853ecaabb9cc46e87bfe0f31523fd9706553e4e
sampe <barrigaete@gmail.com>
2021-09-12 13:50:28 +0800

    convert functional component to class component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,13 +4,15 @@ import { Route } from "react-router-dom";
 import collectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={collectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
+class ShopPage extends React.Component{
+  render(){
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={collectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      </div>
+    );
+  }
 };
 
 export default ShopPage;
