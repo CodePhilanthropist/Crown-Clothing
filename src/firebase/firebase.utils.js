3f95632d020fdec95cdbddb52bccddbfb27af30f
sampe <barrigaete@gmail.com>
2021-09-12 10:30:00 +0800

    format code

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -45,17 +45,20 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = firestore.collection(collectionKey);
   console.log(collectionRef);
 
   const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
+  objectsToAdd.forEach((obj) => {
     const newDocRef = collectionRef.doc();
     batch.set(newDocRef, obj);
   });
 
-  return await batch.commit()
+  return await batch.commit();
 };
 
 export const auth = firebase.auth();
