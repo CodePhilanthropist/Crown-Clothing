90262d23c2610fe9f7ab08623a4f287319f2c7b1
sampe <barrigaete@gmail.com>
2021-09-12 13:51:32 +0800

    format code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,16 +4,19 @@ import { Route } from "react-router-dom";
 import collectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-class ShopPage extends React.Component{
-  render(){
-    const {match} = this.props;
+class ShopPage extends React.Component {
+  render() {
+    const { match } = this.props;
     return (
       <div className="shop-page">
         <Route exact path={`${match.path}`} component={collectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
       </div>
     );
   }
-};
+}
 
 export default ShopPage;
