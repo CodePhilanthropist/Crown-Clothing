ff19fb9ac855e7fe30d4161fba1c321a64c0559e
sampe <barrigaete@gmail.com>
2021-09-12 13:55:08 +0800

    import firestore from firebase utils

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,12 +4,14 @@ import { Route } from "react-router-dom";
 import collectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
+import {firestore} from "../../firebase/firebase.utils";
+
 class ShopPage extends React.Component {
 
   unsubscribeFromSnapshot = null;
 
   componentDidMount(){
-    
+
   }
 
   render() {
