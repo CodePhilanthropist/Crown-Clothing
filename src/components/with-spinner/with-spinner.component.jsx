8571486021ff50d922c45e13134e69e86ba87fb7
sampe <barrigaete@gmail.com>
2021-09-13 14:13:18 +0800

    add Spinner Container

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -4,7 +4,7 @@ import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 const WithSpinner = WrappedComponent => ({isLoading, ...otherProps}) => {
     return isLoading ? (
         <SpinnerOverlay>
-            
+            <SpinnerContainer/>
         </SpinnerOverlay>
     )
 }
