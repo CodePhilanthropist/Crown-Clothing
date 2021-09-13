23692d54428be0c5f4dc984b71c4cc4e71f9cd0b
sampe <barrigaete@gmail.com>
2021-09-13 14:02:11 +0800

    format code

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -14,7 +14,6 @@ export const selectCollectionsForPreview = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections ? collections[collectionUrlParam] : null
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   );
