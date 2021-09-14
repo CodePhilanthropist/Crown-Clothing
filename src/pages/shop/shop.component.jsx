68e6a5530219eca72b298145fb7ba860bc675e6c
sampe <barrigaete@gmail.com>
2021-09-14 08:09:42 +0800

    converting the collectionRef to a promise

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -27,7 +27,7 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
-    collectionRef.get()(async (snapshot) => {
+    collectionRef.get().then(async (snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
