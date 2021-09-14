342bfc8d6ea310fc53c889bd823db09528eb5a94
sampe <barrigaete@gmail.com>
2021-09-15 07:55:38 +0800

    change the updateCollections to fetchCollections

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,6 @@
 import shopActionTypes from "./shop.types";
 
-export const updateCollections = (collectionsMap) => ({
+export const fetchCollectionsStart = (collectionsMap) => ({
   type: shopActionTypes.UPDATE_COLLECTIONS,
   payload: collectionsMap,
 });
