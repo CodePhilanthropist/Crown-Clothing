4fb4c4d4705595840fa75c407c49ce2a927be13a
sampe <barrigaete@gmail.com>
2021-09-15 07:57:44 +0800

    define fetchCollectionsStartAsync

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -3,3 +3,9 @@ import shopActionTypes from "./shop.types";
 export const fetchCollectionsStart = () => ({
   type: shopActionTypes.FETCH_COLLECTIONS_START,
 });
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    
+  }
+}
