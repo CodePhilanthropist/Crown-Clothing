1b08e9bb15eabdd6f01652a73a62385effd7c65b
sampe <barrigaete@gmail.com>
2021-09-17 23:16:38 +0800

    re add fetchCollectionSuccess

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -15,7 +15,7 @@ export const fetchCollectionsStartAsync = () => {
     dispatch(fetchCollectionsStart())
     collectionRef.get().then(async (snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      // // updateCollections(collectionsMap);
+      updateCollections(collectionsMap);
       this.setState({ loading: false });
     });
   };
