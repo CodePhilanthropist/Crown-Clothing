6b3436b09999d32132e3ddfa63e5b430197fab54
sampe <barrigaete@gmail.com>
2021-09-17 23:18:52 +0800

    remove the setState

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -14,10 +14,11 @@ export const fetchCollectionsStartAsync = () => {
   return (dispatch) => {
     const collectionRef = firestore.collection("collections");
     dispatch(fetchCollectionsStart());
+    
     collectionRef.get().then(async (snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       dispatch(fetchCollectionsSuccess(collectionsMap));
-      this.setState({ loading: false });
+      
     });
   };
 };
