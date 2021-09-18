eff29103e29d2c7f9095e1ce9abb2fadb13dac8e
sampe <barrigaete@gmail.com>
2021-09-18 15:29:04 +0800

    format document

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -8,27 +8,27 @@ export const fetchCollectionsStart = () => ({
   type: shopActionTypes.FETCH_COLLECTIONS_START,
 });
 
-
 export const fetchCollectionsSuccess = (collectionsMap) => ({
   type: shopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
 });
 
-export const fetchCollectionFailure = errorMessage => ({
+export const fetchCollectionFailure = (errorMessage) => ({
   type: shopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage,
-
-})
+});
 
 export const fetchCollectionsStartAsync = () => {
   return (dispatch) => {
     const collectionRef = firestore.collection("collections");
     dispatch(fetchCollectionsStart());
 
-    collectionRef.get().then(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchCollectionsSuccess(collectionsMap));
-      
-    }).catch(error => dispatch(fetchCollectionFailure(error.message)))
+    collectionRef
+      .get()
+      .then(async (snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionFailure(error.message)));
   };
 };
