38cbaacbfacb7f27ddae57abb8d598436ef2096a
sampe <barrigaete@gmail.com>
2021-09-19 15:47:34 +0800

    import higher order component WithSpinner

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -2,3 +2,4 @@ import {connect} from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+import WithSpinner from "../with-spinner/with-spinner.component";
