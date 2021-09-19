dee2d474795eafe0527715455677f164540e7f55
sampe <barrigaete@gmail.com>
2021-09-19 15:48:24 +0800

    import collections overview

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -3,4 +3,4 @@ import { createStructuredSelector } from "reselect";
 
 import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
 import WithSpinner from "../with-spinner/with-spinner.component";
-import collectionsOverviewComponent from "./collections-overview.component";
+import collectionsOverview from "./collections-overview.component";
