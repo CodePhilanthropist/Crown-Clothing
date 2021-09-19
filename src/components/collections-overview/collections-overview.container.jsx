2e46b59fe4e41b3a5337a6fc3c2b870ae57761e4
sampe <barrigaete@gmail.com>
2021-09-19 15:47:59 +0800

    importing collectionsOverview

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -3,3 +3,4 @@ import { createStructuredSelector } from "reselect";
 
 import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
 import WithSpinner from "../with-spinner/with-spinner.component";
+import collectionsOverviewComponent from "./collections-overview.component";
