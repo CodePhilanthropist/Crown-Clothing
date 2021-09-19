61c8f472f9ffb0b1e50d4f795656de4facc1bd8b
sampe <barrigaete@gmail.com>
2021-09-19 16:49:58 +0800

    import WithSpinner

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -2,3 +2,4 @@ import { connect } from "react-redux";
 import { compose } from "redux";
 import { createStructuredSelector } from "reselect";
 import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
