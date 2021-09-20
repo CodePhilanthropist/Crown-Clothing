f7adcd061ce91499d16cfcdbfd330eecb4cf9546
sampe <barrigaete@gmail.com>
2021-09-21 07:56:06 +0800

    comment out unnecessary codes

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -12,13 +12,13 @@ import { fetchCollectionsSuccess, fetchCollectionFailure } from "./shop.actions"
 export function* fetchCollectionsAsync() {
   yield console.log("I am fire");
   const collectionRef = firestore.collection("collections");
-  collectionRef
-    .get()
-    .then(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchCollectionsSuccess(collectionsMap));
-    })
-    .catch((error) => dispatch(fetchCollectionFailure(error.message)));
+  // collectionRef
+  //   .get()
+  //   .then(async (snapshot) => {
+  //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+  //     dispatch(fetchCollectionsSuccess(collectionsMap));
+  //   })
+  //   .catch((error) => dispatch(fetchCollectionFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {
