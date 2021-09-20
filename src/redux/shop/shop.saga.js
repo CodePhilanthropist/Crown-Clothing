f85ac34300ccf1cf876939f17517b97f80a023d8
sampe <barrigaete@gmail.com>
2021-09-20 16:01:35 +0800

    yielding the console log for test purposes

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -3,7 +3,7 @@ import { takeEvery } from "redux-saga/effects";
 import shopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync(){
-    console.log("I am fire")
+    yield console.log("I am fire")
 }
 
 export function* fetchCollectionsStart(){
