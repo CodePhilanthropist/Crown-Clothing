a9047981f05cc38ed2efd7372b7d597934ba3345
sampe <barrigaete@gmail.com>
2021-09-20 17:34:25 +0800

    add the code from shop actions to the sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -4,6 +4,13 @@ import shopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync() {
   yield console.log("I am fire");
+  collectionRef
+  .get()
+  .then(async (snapshot) => {
+    const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    dispatch(fetchCollectionsSuccess(collectionsMap));
+  })
+  .catch((error) => dispatch(fetchCollectionFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {
