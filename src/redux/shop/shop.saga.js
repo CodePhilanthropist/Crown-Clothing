6d5c742a8feef185c8d1465637d0ed103cc5f8e7
sampe <barrigaete@gmail.com>
2021-09-20 16:00:54 +0800

    defining fetchCollectionsAsync for async codes og saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -2,6 +2,10 @@ import { takeEvery } from "redux-saga/effects";
 
 import shopActionTypes from "./shop.types";
 
+export function* fetchCollectionsAsync(){
+    
+}
+
 export function* fetchCollectionsStart(){
     yield takeEvery(shopActionTypes.FETCH_COLLECTIONS_START, )
 }
