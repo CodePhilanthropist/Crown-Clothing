4edbd13a7090a02c85d2c01c39c64dbccef54b96
sampe <barrigaete@gmail.com>
2021-09-21 10:56:17 +0800

    yielding put fetchCollectionSuccess

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -21,6 +21,7 @@ export function* fetchCollectionsAsync() {
       convertCollectionsSnapshotToMap,
       snapshot
     );
+    yield put(fetchCollectionsSuccess(collectionsMap))
   } catch (eror) {}
 
   // collectionRef
