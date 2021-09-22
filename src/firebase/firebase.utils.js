9195c2e6e91a0ab2837aa437f16e5db9be6d4ea2
sampe <barrigaete@gmail.com>
2021-09-22 18:00:17 +0800

    export the provider

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -80,7 +80,7 @@ export const convertCollectionsSnapshotToMap = (collections) => {
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
 googleProvider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
