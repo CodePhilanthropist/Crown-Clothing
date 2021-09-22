b971ad7670e2c53724aac4ae1b16cbfd91de461a
sampe <barrigaete@gmail.com>
2021-09-22 15:12:46 +0800

    refactoring the code

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,10 +1,10 @@
-import { UserActionTypes } from './user.types';
+import { UserActionTypes } from "./user.types";
 
-export const setCurrentUser = user => ({
+export const setCurrentUser = (user) => ({
   type: UserActionTypes.SET_CURRENT_USER,
-  payload: user
+  payload: user,
 });
 
 export const googleSignInStart = () => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_START
-})
+  type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
