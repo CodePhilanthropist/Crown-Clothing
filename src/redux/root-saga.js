71b42aab04699099429c17a72298cbc4cb365be6
sampe <barrigaete@gmail.com>
2021-09-22 10:08:30 +0800

    import fetchCollectionsStart

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,7 +1,8 @@
 import  {all, call} from "redux-saga/effects";
+import {fetchCollectionsStart} from "./shop/shop.sagas";
 
 export default function* rootSaga(){
     yield all([
-        
+
     ])
 }
