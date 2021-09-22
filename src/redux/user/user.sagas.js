a0371dd9c66f88b053cb1091cfccc3871741d555
sampe <barrigaete@gmail.com>
2021-09-22 18:36:42 +0800

    putting the googleSignInSuccess

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -13,6 +13,7 @@ export function* signInWithGoogle() {
     const { user } = yield auth.signInWithPopup(googleProvider);
     const userRef = yield call(createUserProfileDocument, user);
     const userSnapshot = yield userRef.get();
+    yield put(googleSignInSuccess({id:userSnapshot.id, ...userSnapshot.data()}))
   } catch (error) {
     console.error(error);
   }
