9af4dd05623592d348e8f570ec9d7bde50ec7f6b
sampe <barrigaete@gmail.com>
2021-09-22 11:48:46 +0800

    import rootSaga

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 import createSagaMiddleware from "redux-saga";
-
+import rootSaga from "./root-saga";
 import { fetchCollectionsStart } from "./shop/shop.sagas";
 
 const sagaMiddleware = createSagaMiddleware();
