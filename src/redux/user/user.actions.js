88e87acccc8b03142b565c0c17531fc65a3f50ee
sampe <barrigaete@gmail.com>
2021-09-22 15:14:42 +0800

    refactor

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -11,5 +11,5 @@ export const googleSignInStart = () => ({
 
 export const googleSignInSuccess = (user) => ({
   type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
-  payload: user
+  payload: user,
 });
