d0d238d85d95bcece6164ba6c8d6076493119339
sampe <barrigaete@gmail.com>
2021-09-23 09:22:06 +0800

    refactor shop reducer

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,30 +1,30 @@
-import shopActionTypes from "./shop.types";
+import ShopActionTypes from './shop.types';
 
 const INITIAL_STATE = {
   collections: null,
   isFetching: false,
-  errorMessage: undefined,
+  errorMessage: undefined
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case shopActionTypes.FETCH_COLLECTIONS_START:
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
       return {
         ...state,
-        isFetching: true,
-      }
-    case shopActionTypes.FETCH_COLLECTIONS_SUCCESS:
-      return {
-        ...state,
-        isFetching: false,
-        collections: action.payload,
+        isFetching: true
       };
-    case shopActionTypes.FETCH_COLLECTIONS_FAILURE:
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
       return {
         ...state,
         isFetching: false,
-        errorMessage: action.payload,
-      }
+        collections: action.payload
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload
+      };
     default:
       return state;
   }
