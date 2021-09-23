aca34ca912ef0b92acf5c05a4c21e29ffb34867d
sampe <barrigaete@gmail.com>
2021-09-23 09:11:36 +0800

    adding exact code

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -28,6 +28,11 @@ export function* signInWithGoogle() {
 export function* signInWithEmail({ payload: { email, password } }) {
   try {
     const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapshot = yield userRef.get();
+    yield put(
+      googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+    );
   } catch (error) {
     yield put(emailSignInFailure(error));
   }
