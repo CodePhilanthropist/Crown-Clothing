b7d0f20af0404aa1b5041afb260e1c4660522e2c
sampe <barrigaete@gmail.com>
2021-09-28 16:24:29 +0800

    passing clearCartOnSignOut as the second parameter

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -7,7 +7,7 @@ export function* clearCartOnSignOut(){
 }
 
 export function* onSignOutSuccess(){
-    yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, )
+    yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut)
 }
 
 export function* cartSagas(){
