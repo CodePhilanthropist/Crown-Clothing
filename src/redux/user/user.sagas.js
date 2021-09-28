e4314cf92bb290aa2555266ad1e005956cb2d98c
sampe <barrigaete@gmail.com>
2021-09-28 15:56:56 +0800

    add additional import

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,6 +1,6 @@
 import { takeLatest, put, all, call } from "redux-saga/effects";
 import UserActionTypes from "./user.types";
-import { signInSuccess, signInFailure, signOutSuccess } from "./user.actions";
+import { signInSuccess, signInFailure, signOutSuccess, signOutFailure } from "./user.actions";
 
 import {
   auth,
