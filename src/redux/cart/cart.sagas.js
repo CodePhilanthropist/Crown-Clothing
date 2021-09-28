d276c586d0330810b3f52c524b90c2c8cbcaf9a6
sampe <barrigaete@gmail.com>
2021-09-28 16:23:11 +0800

    fix the bug

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -2,7 +2,7 @@ import { all, call, takeLatest, put } from "redux-saga/effects";
 import UserActionTypes from "../user/user.types";
 import { clearCart } from "./cart.actions";
 
-export function* onSignOutSuccess{
+export function* onSignOutSuccess(){
     yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS)
 }
 
