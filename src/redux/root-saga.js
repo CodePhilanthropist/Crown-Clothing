cf961205f153f09bed5da896e03823ffef1a0556
sampe <barrigaete@gmail.com>
2021-09-28 16:34:32 +0800

    refactor the code

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -5,5 +5,10 @@ import { cartSagas } from "./cart/cart.sagas";
 import { shopSagas } from "./shop/shop.sagas";
 
 export default function* rootSaga() {
-  yield all([call(fetchCollectionsStart), call(userSagas), call(cartSagas), call(shopSagas)]);
+  yield all([
+    call(fetchCollectionsStart),
+    call(userSagas),
+    call(cartSagas),
+    call(shopSagas),
+  ]);
 }
