54aa4f4a01d3f4cc19155ec8b766907196a8ce8c
sampe <barrigaete@gmail.com>
2021-09-28 15:47:23 +0800

    adding a dispatch

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -44,7 +44,7 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  signOutStart: () => dispatch(signOutStart())
 })
 
 export default connect(mapStateToProps)(Header);
