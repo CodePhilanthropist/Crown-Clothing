2263ebbb9da4827fe84756fb11b3abe9bd60ad64
sampe <barrigaete@gmail.com>
2021-09-28 16:23:01 +0800

    define sign out

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -3,7 +3,7 @@ import UserActionTypes from "../user/user.types";
 import { clearCart } from "./cart.actions";
 
 export function* onSignOutSuccess{
-    
+    yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS)
 }
 
 export function* cartSagas(){
